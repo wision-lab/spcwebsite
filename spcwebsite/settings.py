d2c76d770b13cf341f694db9769ed21f48e724a7
Sacha Jungerman <jungerm2@illinois.edu>
2025-09-23 08:44:41 -0500

    enable pragma journal-mode WAL

--- spcwebsite/settings.py
+++ spcwebsite/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        "OPTIONS": {
+            "init_command": "PRAGMA journal_mode=wal;",
+            "transaction_mode": "IMMEDIATE",
+        },
     }
 }
 
